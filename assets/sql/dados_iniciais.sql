9207f58d3b31716b02596ec3d7ef103cfdf29aa3
luizmiguel05souza <luizmiguelsouzaalves@hotmail.com>
2021-10-01 10:51:43 +0000

    Preparação da Base de Dados

---
+++ assets/sql/dados_iniciais.sql
@@ -0,0 +1,11 @@
+INSERT INTO generos(id, nome) VALUES
+    (1, 'Alta Fantasia'),
+    (2, 'Policial'),
+    (3, 'Drama'),
+    (4, 'Ficção Ciêntifica');
+
+INSERT INTO livros(id, titulo, id_genero) VALUES
+    (1, 'Assasinato no Expresso do Oriente', 2),
+    (2, 'A Sociedade do Anel', 1),
+    (3, 'Como eu Era Antes de Você', 3),
+    (4, 'O Guia do Mochileiro das Galáxias', 4);
